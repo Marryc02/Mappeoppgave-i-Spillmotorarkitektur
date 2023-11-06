57098c6f46f749199d487bd89327fb58fc96365b
Markus Rychter <89453088+Marryc02@users.noreply.github.com>
2023-11-06 18:27:53 +0100

    Checkpoint while working on the main script

--- Assets/Scripts/MainScript.cs
+++ Assets/Scripts/MainScript.cs
@@ -1,25 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+//using System.Numerics;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class MainScript : MonoBehaviour
 {
+
+
+
+    public GameObject wagonPrefab;
+    public GameObject smallTrainPrefab;
+    public GameObject mediumTrainPrefab;
+    public GameObject largeTrainPrefab;
+
+
     string trainType;
+    float trainVelocity;
+    float trainAccelration;
+
+    int wagonAmount;
+    float wagonMass;
+
+
+    List<GameObject> trainAndWagons;
+
+
     // Start is called before the first frame update
     void Start()
     {
         if (trainType == "Small") {
-
+            //Instantiate(smallTrainPrefab, new Vector3(0, 0, 0), quaternion.identity);
+            trainAndWagons.Add(smallTrainPrefab);
+            trainAndWagons[0].GetComponent<TrainClassScript>().trainClassInstance.velocity = trainVelocity;
         }
         else if (trainType == "Medium") {
-
+            //Instantiate(mediumTrainPrefab, new Vector3(0, 0, 0), quaternion.identity);
+            trainAndWagons.Add(mediumTrainPrefab);
+            trainAndWagons[0].GetComponent<TrainClassScript>().velocity = trainVelocity;
         }
         else if (trainType == "Large") {
-
+            //Instantiate(largeTrainPrefab, new Vector3(0, 0, 0), quaternion.identity);
+            trainAndWagons.Add(largeTrainPrefab);
+            trainAndWagons[0].GetComponent<TrainClassScript>().velocity = trainVelocity;
         }
         else {
             Debug.Log("No train type selected!");
+        }
+
+        for (int i = 0; i < wagonAmount; i++)
+        {
+            trainAndWagons.Add(wagonPrefab);
+            trainAndWagons[i].GetComponent<Rigidbody>().mass = wagonMass;
+        }
+
+        for (int i = 0; i < trainAndWagons.Count; i++)
+        {
+            Instantiate(trainAndWagons[i], new Vector3(0, 0, i + i), quaternion.identity);
+
+            if (i != 0)
+            {
+                trainAndWagons[i-1].GetComponent<HingeJoint>().connectedBody = trainAndWagons[i].GetComponent<Rigidbody>();
+            }
         }
     }
 
--- Assets/Scripts/TrainClassScript.cs
+++ Assets/Scripts/TrainClassScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class TrainClassScript : MonoBehaviour
 {
+    public TrainClassScript trainClassInstance;
+
     [SerializeField] float velocity;
     [SerializeField] float acceleration;
     [SerializeField] float givenMass;
