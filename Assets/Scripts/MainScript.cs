144390eb9ffcbe473035a7226ac1da9082d41946
Markus Rychter <89453088+Marryc02@users.noreply.github.com>
2023-11-06 17:40:26 +0100

    Quick push for JKing

--- Assets/Scripts/MainScript.cs
+++ Assets/Scripts/MainScript.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MainScript : MonoBehaviour
 {
+    string trainType;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (trainType == "Small") {
+
+        }
+        else if (trainType == "Medium") {
+
+        }
+        else if (trainType == "Large") {
+
+        }
+        else {
+            Debug.Log("No train type selected!");
+        }
     }
 
     // Update is called once per frame
