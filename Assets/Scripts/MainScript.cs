e87768c1fd0c97abcb43972bfb6a735ef30c3723
Markus Rychter <89453088+Marryc02@users.noreply.github.com>
2023-11-07 17:58:30 +0100

    Switched velocity and acceleration over to the rigidbody values

--- Assets/Scripts/MainScript.cs
+++ Assets/Scripts/MainScript.cs
@@ -14,8 +14,9 @@ public class MainScript : MonoBehaviour
 
 
     string trainType = "Small";
-    float trainVelocity;
-    float trainAcceleration;
+    Vector3 trainVelocity;
+    Vector3 lastVelocity;
+    Vector3 trainAcceleration;
 
     int wagonAmount = 3;
     float wagonMass;
@@ -26,39 +27,48 @@ public class MainScript : MonoBehaviour
 
     void SpawnTrainAndWagons()
     {
+        // Adds a prefab of either type "Small", "Medium" or "Large" to the trainAndWagons-List and assigfns a given velocity.
         if (trainType == "Small") {
             trainAndWagons.Add(smallTrainPrefab);
-            trainAndWagons[0].GetComponent<TrainClassScript>().velocity = trainVelocity;
-            trainAndWagons[0].GetComponent<TrainClassScript>().acceleration = trainAcceleration;
+            trainAndWagons[0].GetComponent<Rigidbody>().velocity = trainVelocity;
         }
         else if (trainType == "Medium") {
             trainAndWagons.Add(mediumTrainPrefab);
-            trainAndWagons[0].GetComponent<TrainClassScript>().velocity = trainVelocity;
-            trainAndWagons[0].GetComponent<TrainClassScript>().acceleration = trainAcceleration;
+            trainAndWagons[0].GetComponent<Rigidbody>().velocity = trainVelocity;
         }
         else if (trainType == "Large") {
             trainAndWagons.Add(largeTrainPrefab);
-            trainAndWagons[0].GetComponent<TrainClassScript>().velocity = trainVelocity;
-            trainAndWagons[0].GetComponent<TrainClassScript>().acceleration = trainAcceleration;
+            trainAndWagons[0].GetComponent<Rigidbody>().velocity = trainVelocity;
         }
+        // Ensures an actual train type is selected.
         else {
             Debug.Log("No train type selected!");
         }
 
+        // Adds wagons based on the specified amount of wagons the user wants to add to the trainAndWagons-List.
         for (int i = 0; i < wagonAmount; i++)
         {
             trainAndWagons.Add(wagonPrefab);
-            //trainAndWagons[i].GetComponent<Rigidbody>().mass = wagonMass;
         }
 
+        // Instantiates the trainAndWagons-List with spacing in-between all the different cubes (train/wagons).
         for (int i = 0; i < trainAndWagons.Count; i++)
         {
             Instantiate(trainAndWagons[i], new Vector3(0, 0, i * 2), quaternion.identity);
 
+            // Connects the rigidbodies of the next object (Wagon) to the previous object's Hinge Joint (Train/Wagon).
             if (i != 0)
             {
                 trainAndWagons[i-1].GetComponent<HingeJoint>().connectedBody = trainAndWagons[i].GetComponent<Rigidbody>();
             }
         }
+    }
+
+    // Generates a value for acceleration every FixedUpdate and applies it to the train's rigidbody's velocity.
+    private void FixedUpdate() {
+        lastVelocity = GetComponent<Rigidbody>().velocity;
+        trainAcceleration = (GetComponent<Rigidbody>().velocity - lastVelocity) / Time.fixedDeltaTime;
+
+        trainAndWagons[0].GetComponent<Rigidbody>().velocity += trainAcceleration;
     }
 }
