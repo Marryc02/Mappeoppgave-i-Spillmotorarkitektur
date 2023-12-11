cc11a1f2854278f9155e6b4ff526a4550e80f148
Markus Rychter <89453088+Marryc02@users.noreply.github.com>
2023-12-11 15:49:42 +0100

    Added most of the physics. I am just waiting for the spline to be fixed so that I can add normals/unitNormals/normalForce into the equation

--- Assets/Scripts/FollowSpline.cs
+++ Assets/Scripts/FollowSpline.cs
@@ -7,6 +7,16 @@ public class FollowSpline : MonoBehaviour
 {
     [SerializeField] SplineAnimate splineAnimate;
 
+    float velocity = MainScript.mainInstance.trainStartingVelocity;
+    float acceleration;
+    float mass = MainScript.mainInstance.trainMass + (MainScript.mainInstance.wagonMass * MainScript.mainInstance.wagonAmount);
+    Vector3 normal;
+    Vector3 unitNormal;
+    float normalForce;
+    float gravity = 9.81f;
+    float gravitationalForce;
+    float sumOfAllForce;
+
     void Awake()
     {
         GameObject splineObject = GameObject.FindGameObjectWithTag("Slope");
@@ -17,10 +27,25 @@ public class FollowSpline : MonoBehaviour
         if (slope != null)
         {
             splineAnimate.Container = slope;
+            splineAnimate.MaxSpeed = velocity;
         }
         else
         {
             Debug.Log("GameObject could not find spline component!");
         }
+    }
+
+    private void FixedUpdate() {
+        gravitationalForce = gravity * mass;
+        // First find the normal in the current point, then do the following:
+            /*
+                unitNormal = normal.Normalize();
+                normalForce = -(gravitationalForce * unitNormal) * unitNormal;
+            */
+        //
+        sumOfAllForce = gravitationalForce + normalForce;
+        acceleration = sumOfAllForce / mass;
+
+        splineAnimate.MaxSpeed += acceleration;
     }
 }
